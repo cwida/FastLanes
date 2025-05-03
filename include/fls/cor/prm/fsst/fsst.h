84184c0ee0b15271fabab2edb8dcd02af777d80d
azimafroozeh <afroozeh3@gmail.com>
2025-05-03 12:48:07 +0200

    format

--- include/fls/cor/prm/fsst/fsst.h
+++ include/fls/cor/prm/fsst/fsst.h
@@ -58,7 +58,7 @@
 
 #ifdef _MSC_VER
 #define __restrict__
-#define __BYTE_ORDER__          __ORDER_LITTLE_ENDIAN__
+#define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
 // #define __ORDER_LITTLE_ENDIAN__ 2
 #include <intrin.h>
 static inline int __builtin_ctzl(unsigned long long x) {
