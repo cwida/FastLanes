6e9036df38a97b669b5e80ce786ba5c51d626159
azimafroozeh <afroozeh3@gmail.com>
2025-05-04 13:11:45 +0200

    use issue::ISSUE_000 as an example

--- examples/cpp_example.cpp
+++ examples/cpp_example.cpp
@@ -9,7 +9,7 @@ int main() {
 
 	try {
 		Connection     con1;
-		const path     example_dir_path = string(GENERATED::ANY_VALUE_COUNT_I64_666);
+		const path     example_dir_path = string(issue::ISSUE_000);
 		const path     fls_dir_path     = path {FLS_CMAKE_SOURCE_DIR} / "data" / "fls";
 		const path     csv_file_path    = fls_dir_path / "fastlanes.csv";
 		const fs::path fls_file_path    = fls_dir_path / "data.fls";
