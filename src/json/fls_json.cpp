3fdc8185f68f7c1cd4880acf728101b310ca6c48
azimafroozeh <afroozeh3@gmail.com>
2025-05-27 11:19:31 +0200

    add nextiajd test

--- src/json/fls_json.cpp
+++ src/json/fls_json.cpp
@@ -229,6 +229,11 @@ DataType TypeLookUp(const std::string& str) {
 	    {"varchar(950)", DataType::FLS_STR},
 	    {"varchar(96)", DataType::FLS_STR},
 	    {"varchar(99)", DataType::FLS_STR},
+
+	    // NextiaJD
+	    {"TIMESTAMP", DataType::FLS_STR},
+	    {"BOOLEAN", DataType::FLS_STR},
+
 	};
 
 	const auto it = TABLE.find(str);
@@ -302,7 +307,7 @@ std::string to_string(const std::vector<std::unique_ptr<ExpressionResultT>>& pai
 	for (const auto& ptr : pairs) {
 		if (!ptr) {
 			continue;
-		}                                          // defensive: skip nulls
+		} // defensive: skip nulls
 		const auto& [operator_token, size] = *ptr; // structured-bind the pointed-to pair
 
 		if (!first) {
@@ -474,7 +479,7 @@ void from_json(const nlohmann::json& j, TableDescriptorT& table_descriptor) {
 constexpr const auto* OPERATOR_TOKEN = "1  [REQUIRED], OPERATOR_TOKEN";
 constexpr const auto* SIZE           = "2  [REQUIRED], SIZE";
 void                  to_json(nlohmann::json& j, const ExpressionResultT& expression_result) {
-	                 j = nlohmann::json {
+    j = nlohmann::json {
         //
         {OPERATOR_TOKEN, expression_result.operator_token}, //
         {SIZE, expression_result.size},                     //
