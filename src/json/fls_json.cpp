c253dd72174283d75e30a0c69ac6a5db47691225
azimafroozeh <afroozeh3@gmail.com>
2025-05-27 13:50:06 +0200

    add nextiajd tests

--- src/json/fls_json.cpp
+++ src/json/fls_json.cpp
@@ -233,6 +233,7 @@ DataType TypeLookUp(const std::string& str) {
 	    // NextiaJD
 	    {"TIMESTAMP", DataType::FLS_STR},
 	    {"BOOLEAN", DataType::FLS_STR},
+	    {"DATE", DataType::FLS_STR},
 
 	};
 
@@ -307,7 +308,7 @@ std::string to_string(const std::vector<std::unique_ptr<ExpressionResultT>>& pai
 	for (const auto& ptr : pairs) {
 		if (!ptr) {
 			continue;
-		} // defensive: skip nulls
+		}                                          // defensive: skip nulls
 		const auto& [operator_token, size] = *ptr; // structured-bind the pointed-to pair
 
 		if (!first) {
@@ -479,7 +480,7 @@ void from_json(const nlohmann::json& j, TableDescriptorT& table_descriptor) {
 constexpr const auto* OPERATOR_TOKEN = "1  [REQUIRED], OPERATOR_TOKEN";
 constexpr const auto* SIZE           = "2  [REQUIRED], SIZE";
 void                  to_json(nlohmann::json& j, const ExpressionResultT& expression_result) {
-    j = nlohmann::json {
+	                 j = nlohmann::json {
         //
         {OPERATOR_TOKEN, expression_result.operator_token}, //
         {SIZE, expression_result.size},                     //
--- test/src/dataset_tests/nextiajd_test.cpp
+++ test/src/dataset_tests/nextiajd_test.cpp
@@ -4,208 +4,206 @@
 
 namespace fastlanes {
 
-// Converted NextiaJD tables into FastLanesReaderTester format
-
-TEST_F(FastLanesReaderTester, Chicago_Crimes_2001_to_2004) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_Chicago_Crimes_2001_to_2004) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::Chicago_Crimes_2001_to_2004, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, Chicago_Crimes_2005_to_2007) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_Chicago_Crimes_2005_to_2007) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::Chicago_Crimes_2005_to_2007, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, Chicago_Crimes_2012_to_2017) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_Chicago_Crimes_2012_to_2017) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::Chicago_Crimes_2012_to_2017, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, Consumer_Complaints) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_Consumer_Complaints) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::Consumer_Complaints, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, Homo_sapiens_GRCh38_92) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_Homo_sapiens_GRCh38_92) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::Homo_sapiens_GRCh38_92, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, all_anonymized_2015_11_2017_03) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_all_anonymized_2015_11_2017_03) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::all_anonymized_2015_11_2017_03, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, artists) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_artists) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::artists, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, az) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_az) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::az, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, business_licences) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_business_licences) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::business_licences, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, comments) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_comments) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::comments, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, econbiz) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_econbiz) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::econbiz, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, eo2) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_eo2) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::eo2, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, eo3) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_eo3) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::eo3, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, examiner_date_text) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_examiner_date_text) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::examiner_date_text, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, examiner_date_tokens) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_examiner_date_tokens) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::examiner_date_tokens, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, glassdoor_photos) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_glassdoor_photos) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::glassdoor_photos, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, news_week_17aug24) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_news_week_17aug24) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::news_week_17aug24, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, otp) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_otp) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::otp, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, plays) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_plays) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::plays, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, previous_application) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_previous_application) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::previous_application, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, property_tax_report) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_property_tax_report) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::property_tax_report, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, reviews_detailed) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_reviews_detailed) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::reviews_detailed, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, reviews_summary) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_reviews_summary) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::reviews_summary, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, saudi_hourly_weather_data) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_saudi_hourly_weather_data) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::saudi_hourly_weather_data, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, train_balanced_sarcasm) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_train_balanced_sarcasm) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::train_balanced_sarcasm, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, trainView) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_trainView) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::trainView, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, us_perm_visas) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_us_perm_visas) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::us_perm_visas, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, wowah_data) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_wowah_data) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::wowah_data, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
-TEST_F(FastLanesReaderTester, yellow_tripdata_2019_01) {
-	const vector<n_t>     constant_cols                 = {};
-	constexpr vector<n_t> equal_cols                    = {};
-	const vector<n_t>     one_to_one_mapped_col_indexes = {};
+TEST_F(FastLanesReaderTester, NextiaJD_yellow_tripdata_2019_01) {
+	const vector<n_t> constant_cols                 = {/* ... */};
+	const vector<n_t> equal_cols                    = {/* ... */};
+	const vector<n_t> one_to_one_mapped_col_indexes = {/* ... */};
 	AllTest(NextiaJD::yellow_tripdata_2019_01, constant_cols, equal_cols, one_to_one_mapped_col_indexes);
 }
 
