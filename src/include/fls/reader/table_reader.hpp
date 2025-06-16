62d41437594bd493538258f6dbb808c856bc0cad
Sebastiaan Gerritsen <dev@sebastiaan.io>
2025-06-16 13:31:28 +0200

    Update Table Reader

--- src/include/fls/reader/table_reader.hpp
+++ src/include/fls/reader/table_reader.hpp
@@ -23,6 +23,9 @@ public:
 	[[nodiscard]] up<RowgroupReader> get_rowgroup_reader(n_t rowgroup_idx) const;
 	//
 	[[nodiscard]] up<Table> materialize() const;
+	[[nodiscard]] TableDescriptorT& get_file_metadata() const;
+	[[nodiscard]] n_t get_n_rowgroups() const;
+
 	// API: append to the existing csv.
 	// write to the fle path
 	void to_csv(const path& file_path) const;
