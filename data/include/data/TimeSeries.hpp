6764bd0593456ede866fdd150c3d3714927ea0d3
yuxin370 <yuxintang370@gmail.com>
2025-10-15 12:18:07 +0800

    fix dir

--- data/include/data/TimeSeries.hpp
+++ data/include/data/TimeSeries.hpp
@@ -16,11 +16,11 @@ using timeseries_dataset_t = std::array<std::pair<std::string_view, std::string_
 // clang-format off
 class TimeSeries {
 public:   
-    static constexpr std::string_view AMPds                             { FASTLANES_DATA_DIR "/data/time_series/tables/AMPds" };                    
-    static constexpr std::string_view WEATHER_FORCAST                   { FASTLANES_DATA_DIR "/data/time_series/tables/Weather_Forcast" };           
-    static constexpr std::string_view SMART_GRID                        { FASTLANES_DATA_DIR "/data/time_series/tables/Smart_Grid" };               
-    static constexpr std::string_view LINEAR_ROEAD                      { FASTLANES_DATA_DIR "/data/time_series/tables/Linear_Road" };               
-    static constexpr std::string_view COMPUTER_MONITOR                  { FASTLANES_DATA_DIR "/data/time_series/tables/Computer_Monitor" };  
+    static constexpr std::string_view AMPds                             { FASTLANES_DATA_DIR "/time_series/tables/AMPds" };                    
+    static constexpr std::string_view WEATHER_FORCAST                   { FASTLANES_DATA_DIR "/time_series/tables/Weather_Forcast" };           
+    static constexpr std::string_view SMART_GRID                        { FASTLANES_DATA_DIR "/time_series/tables/Smart_Grid" };               
+    static constexpr std::string_view LINEAR_ROEAD                      { FASTLANES_DATA_DIR "/time_series/tables/Linear_Road" };               
+    static constexpr std::string_view COMPUTER_MONITOR                  { FASTLANES_DATA_DIR "/time_series/tables/Computer_Monitor" };  
     static constexpr timeseries_dataset_t dataset = {{
         { "AMPds",                           AMPds },
         { "Weather_Forcast",                 WEATHER_FORCAST },
