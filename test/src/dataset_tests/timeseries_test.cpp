befb7ecd6b1bc26f7aa54583025991d8a8dfc3a6
yuxin370 <yuxintang370@gmail.com>
2025-10-15 09:32:34 +0800

    style: format timeseries_test.cpp

--- test/src/dataset_tests/timeseries_test.cpp
+++ test/src/dataset_tests/timeseries_test.cpp
@@ -9,21 +9,19 @@
 
 namespace fastlanes {
 
-#define TIMESERIES_TEST(DATASET_VAR)                                                                                     \
-	TEST_F(FastLanesReaderTester, TimeSeries_##DATASET_VAR) {                                                            \
+#define TIMESERIES_TEST(DATASET_VAR)                                                                                   \
+	TEST_F(FastLanesReaderTester, TimeSeries_##DATASET_VAR) {                                                          \
 		const std::vector<n_t> constant_cols {};                                                                       \
 		const std::vector<n_t> equal_cols {};                                                                          \
 		const std::vector<n_t> one_to_one_mapped_col_indexes {};                                                       \
-		AllTest(TimeSeries::DATASET_VAR, constant_cols, equal_cols, one_to_one_mapped_col_indexes);                      \
+		AllTest(TimeSeries::DATASET_VAR, constant_cols, equal_cols, one_to_one_mapped_col_indexes);                    \
 	}
-
 
 TIMESERIES_TEST(SMART_GRID)
 TIMESERIES_TEST(LINEAR_ROEAD)
 TIMESERIES_TEST(COMPUTER_MONITOR)
 TIMESERIES_TEST(AMPds)
 TIMESERIES_TEST(WEATHER_FORCAST)
-
 
 #undef TIMESERIES_TEST
 
